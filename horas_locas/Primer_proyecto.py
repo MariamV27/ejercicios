8e75a7301d61d64f3d87290abf9e00bd081a27d0
Mariam Velasquez <mariamvelasquez27@gmail.com>
2023-12-26 01:01:45 -0300

    agregue las carpetas al repositorio.

---
+++ horas_locas/Primer_proyecto.py
@@ -0,0 +1,11 @@
+#Concatenar cadenas de caracteres.
+
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural1 = input("Sustantivo (plural): ")
+
+madlib = f" Me encantan las tortas mis favoritas son {adj} Siempre me emociona porque me encanta {verbo1} problemas. Aprende a {verbo2} con freeCodeCamp y alcanzo tus {sustantivo_plural1}! "
+
+#Print
+print(madlib)
---
+++ horas_locas/adivina_numero.py
@@ -0,0 +1,16 @@
+# Funcion
+
+import random
+
+def adivina_el_número(x):
+    print("===================== ")
+    print("Bienvenido al juego ")
+    print("===================== ")
+    print("Tu meta es adivinar el número generado por la computadora. ")
+
+ numero_aleatorio = random.randint(1, x)
+
+prediccion = 0
+
+while prediccion != numero_aleatorio
+    prediccion = input(f"")
